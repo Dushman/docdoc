bf1985754670bae32e22785d97ff690aa7839054
Dushman <ebanniynasos@gmail.com>
2014-08-11 14:16:35 +0300

    1

--- public/js/script.js
+++ public/js/script.js
@@ -54,7 +54,7 @@ $(function() {
     });
   });
   hideHeader = function() {
-    if ($(window).height() <= 560) {
+    if ($(window).height() <= 400) {
       return $(document).on('scrollstart', function() {
         $('.hide-scroll').css('display', 'none');
         return $('#find-doctor div[data-role=\"header\"]').toolbar('updatePagePadding');
@@ -64,6 +64,5 @@ $(function() {
       });
     }
   };
-  hideHeader();
-  return alert($(window).height());
+  return hideHeader();
 });
